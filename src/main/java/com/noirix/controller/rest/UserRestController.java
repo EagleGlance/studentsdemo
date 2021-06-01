0798d4ebe92e8d8a7e512bc55014c15db33e4772
Viachaslau Kalevich <cto@noirix.com>
2021-06-01 21:13:17 +0300

    Spring Rest with Exception Handling

--- src/main/java/com/noirix/controller/rest/UserRestController.java
+++ src/main/java/com/noirix/controller/rest/UserRestController.java
@@ -1,12 +1,22 @@
 package com.noirix.controller.rest;
 
+import com.noirix.beans.SecurityConfig;
+import com.noirix.controller.requests.UserCreateRequest;
 import com.noirix.domain.User;
 import com.noirix.repository.UserRepository;
+import com.noirix.util.UserGenerator;
 import lombok.RequiredArgsConstructor;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.HttpServletRequest;
+import java.util.Collections;
 import java.util.List;
 
 @RestController
@@ -15,10 +25,48 @@ import java.util.List;
 public class UserRestController {
 
     private final UserRepository userRepository;
+    private final UserGenerator userGenerator;
+    private final SecurityConfig config;
 
     @GetMapping
     public List<User> findAll() {
         System.out.println("In rest controller");
         return userRepository.findAll();
     }
+
+    @GetMapping("/hello")
+    public List<User> securedFindAll(HttpServletRequest request) {
+        String secretKey = request.getHeader("Secret-Key");
+
+        if (StringUtils.isNotBlank(secretKey) && secretKey.equals(config.getSecretKey())) {
+            return userRepository.findAll();
+        } else {
+            //throw new UnauthorizedException();
+            return Collections.emptyList();
+        }
+    }
+
+    @GetMapping("/search")
+    public List<User> userSearch(@RequestParam Integer limit, @RequestParam String query) {
+        return userRepository.findUsersByQuery(limit, query);
+    }
+
+    @PostMapping
+    public User createUser(@ModelAttribute UserCreateRequest createRequest) {
+        User generatedUser = userGenerator.generate();
+        generatedUser.setWeight(createRequest.getWeight());
+        generatedUser.setLogin(createRequest.getLogin());
+
+        return userRepository.save(generatedUser);
+    }
+
+    @PostMapping("/generate/{usersCount}")
+    public List<User> generateUsers(@PathVariable("usersCount") Integer count) {
+        throw new RuntimeException("Haha!");
+//        List<User> generateUsers = userGenerator.generate(count);
+//        userRepository.batchInsert(generateUsers);
+//
+//        return userRepository.findAll();
+    }
+
 }
